8c24344200442e8f074d44b3df3d0be629e5b721
omairAR <omairrahman195@gmail.com>
2021-10-08 15:41:09 +0530

    home page added

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+
+
+
+    {/* Navlink uses [ to ] to route to page click */}
+    {/* use exact in order to match same component */}
+
+    return (
+        <p>hello to home</p>
+        
+    )
+}
+
+export default Home;
